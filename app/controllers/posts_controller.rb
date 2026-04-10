de8d10f0899be93360c0683617d4e9ac3d6826bb
jbrenesp <josembrenes@gmail.com>
2026-04-10 20:20:39 +0300

    Add image upload to posts with Active Storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -79,6 +79,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.expect(post: [ :title, :body ])
+      params.expect(post: [ :title, :body, :image ])
     end
 end

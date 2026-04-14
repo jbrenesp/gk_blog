3cf373902fecdc8929726daf3809297797f7fba7
jbrenesp <josembrenes@gmail.com>
2026-04-14 20:19:24 +0300

    Enable secure password authentication with bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_10_170136) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_14_171158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_10_170136) do
     t.datetime "created_at", null: false
     t.integer "likes", default: 0
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 

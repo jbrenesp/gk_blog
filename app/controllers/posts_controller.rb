ed06a978ec177aba8944b9d0d58157fd8ef82524
jbrenesp <josembrenes@gmail.com>
2026-04-16 16:29:24 +0300

    Associate posts with users (belongs_to / has_many)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,16 +21,12 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+    redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 

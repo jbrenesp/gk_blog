8e99a1a5e50c51a615dce7819317b28cc4c4d39e
jbrenesp <josembrenes@gmail.com>
2026-04-16 16:51:31 +0300

    Add published field to posts (draft system foundation)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_16_133833) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_16_134551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
+    t.boolean "published"
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false

ba7cbbc9a914cc7006a64becf3bcba2b07653d13
jbrenesp <josembrenes@gmail.com>
2026-04-23 14:50:16 +0300

    Add CSS polish, sticky navbar, footer, post actions layout and Action Text install

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.where(status: :published)
+    @posts = Post.where(status: :published).order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json

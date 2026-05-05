25c93ca29684242bbbb204ce24b46214902d1ae8
jbrenesp <josembrenes@gmail.com>
2026-05-05 13:40:36 +0300

    Add functional admin dashboard with basic analytics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_01_121943) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_05_103229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_01_121943) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "views_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 

3536af1237b9e20773075d1560e0ce6735cd262d
jbrenesp <josembrenes@gmail.com>
2026-05-20 13:19:16 +0300

    Fix security warnings and stabilize UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-
-    @user.role ||= :author
+    # admin decides role (default fallback if not provided)
+    @user.role = params[:user][:role].presence || "author"
 
     if @user.save
-      redirect_to root_path, notice: "User created successfully"
+      redirect_to users_path, notice: "User created successfully"
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,7 +25,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
+    # update normal fields
     if @user.update(user_params)
+
+      @user.update(role: params[:user][:role]) if params[:user][:role].present?
+
       redirect_to users_path, notice: "User updated successfully"
     else
       redirect_to users_path, alert: "Could not update user"
@@ -43,6 +47,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :role)
+    params.require(:user).permit(:name, :email, :password)
   end
 end

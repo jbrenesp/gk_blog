7aaed6c5346a85faca38d59e2f026db71c60388a
jbrenesp <josembrenes@gmail.com>
2026-07-09 15:10:07 +0300

    Strip Action Text attachment metadata from post excerpts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_rich_text :body
+
+    def preview_image
+      body.embeds.first
+    end
+
+    def plain_excerpt
+        fragment = Nokogiri::HTML::DocumentFragment.parse(body.to_s)
+        fragment.css("action-text-attachment").remove
+        ActionView::Base.full_sanitizer.sanitize(fragment.to_html).squish
+    end
 end
